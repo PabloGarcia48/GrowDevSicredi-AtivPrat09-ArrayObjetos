063d6d3bfd97f22e203727670fdf4d28abdb72e5
Pablo Garcia <pablorobertodev@gmail.com>
2023-11-07 17:52:59 -0300

    First commit

---
+++ prat09Exer2.js
@@ -0,0 +1,17 @@
+/*
+2. Crie um objeto em JavaScript para colocar 5 atributos de um
+notebook. Atribua os seguintes atributos:
+*/
+
+const computador = {
+    processador: "i7",
+    memoria: "16GB",
+    preco: 5000,
+    HD: "1TB",
+    SSD: "256GB"
+}
+document.write(`Processador = ${computador.processador}<br>`);
+document.write(`Memória = ${computador.memoria}<br>`);
+document.write(`Preço = ${computador.preco}<br>`);
+document.write(`HD = ${computador.HD}<br>`);
+document.write(`SSD = ${computador.SSD}<br>`);
